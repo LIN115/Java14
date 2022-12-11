a282366b01088df831fc0d27fc91a61db750b446
LIN115 <113489076+LIN115@users.noreply.github.com>
2022-12-11 15:43:27 +0800

    Add files via upload

---
+++ Class01.java
@@ -0,0 +1,11 @@
+package Java14;
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//InputStream、OutputStream 類別是用來處理「位元串流」,也就是二進位檔,同時它
+//也可以處理純文字檔。Reader、Writer 類別則是用來處理「字元串流」,也就是純文字檔。
+	}
+
+}
